fe79ac15f0bbb9264cc14b9df5ab50b08c17b45b
zoheb598 <joheb133@gmail.com>
2024-03-07 15:33:15 +0000

    [feat] add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div className='navbar'>
+            <span className='text-brand-primary text-4xl'>J</span>
+            <ol className='text-white flex justify-between'>
+                <li><a href="" className='p-2'>About</a></li>
+                <li><a href="" className='p-2'>Projects</a></li>
+                <li><a href="" className='p-2'>Contact</a></li>
+            </ol>
+            <button className='bg-white rounded-full m-[2px] w-[134px] h-[44px] ml-auto'>
+                CV
+            </button>
+        </div>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'brand-primary': '#00D42F', // Custom name approach 
+            }
+        },
     },
     plugins: [],
 }

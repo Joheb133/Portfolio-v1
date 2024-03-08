ca87c6609d9e74f008da023ec681f7846a7b5295
zoheb598 <joheb133@gmail.com>
2024-03-08 14:39:21 +0000

    [style] improve navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,23 @@ import './index.css'
 
 function App() {
 
-  return (
-    <>
-        <div className='navbar'>
-            <span className='text-brand-primary text-4xl'>J</span>
-            <ol className='text-white flex justify-between'>
-                <li><a href="" className='p-2'>About</a></li>
-                <li><a href="" className='p-2'>Projects</a></li>
-                <li><a href="" className='p-2'>Contact</a></li>
-            </ol>
-            <button className='bg-white rounded-full m-[2px] w-[134px] h-[44px] ml-auto'>
-                CV
-            </button>
-        </div>
-    </>
-  )
+    return (
+        <>
+            <div className='navbar'>
+                <a href="" className='text-brand-primary text-4xl font-mono font-bold mx-8'>J</a>
+                <div className='flex items-center justify-center gap-6'>
+                    <ol className='links text-white flex justify-between gap-4'>
+                        <li><a href="">About</a></li>
+                        <li><a href="">Projects</a></li>
+                        <li><a href="">Contact</a></li>
+                    </ol>
+                    <button className='bg-white rounded-full m-[2px] w-[112px] h-[52px]'>
+                        CV
+                    </button>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App
